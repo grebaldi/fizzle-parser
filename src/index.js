d0da519b46b1e5c34e2da90d50d3cc8cbb86f708
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-03-08 13:14:04 +0100

    TASK: Create fizzle filter skeleton

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-var parser = require('./fizzle-parser.js');
-var util = require('util');
-var myObject = parser.parse('[test="something"],[blubb>=12],[instanceof TYPO3.Neos:Content]');
+import parser from './fizzle.js';
 
-console.log(util.inspect(myObject, false, null));
+export {parser};
+
+const matchesFilterGroup = () => false;
+const matchesFilter = () => false;
+const matchesPropertyNameFilter = () => false;
+const matchesAttributeFilter = () => false;
+const matchesIdentifierFilter = () => false;
+
+const filterObject = expr => subject => {
+
+};
+
+const filterArray = expr => subject => {
+
+};
+
+export const filter = fizzleExpression => subject => {
+
+};
